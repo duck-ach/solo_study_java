2cbd6d0270ffa68e04c91723d828381fbfda635a
duck_ach <djagmlfk@naver.com>
2022-06-03 22:48:00 +0900

    01-3 Programing java

---
+++ chap1/src/sec03/exam01/Hello.java
@@ -0,0 +1,11 @@
+package sec03.exam01;
+
+public class Hello {
+// 프로그램 실행 진입점 
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문 
+		System.out.println("Hello Java");
+
+	} //end of main
+
+} //end of class
