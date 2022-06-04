de008a195b5b86d109ebc67c73b0f1f6e45b64e6
duck_ach <djagmlfk@naver.com>
2022-06-04 15:40:18 +0900

    variable

---
+++ chap1/src/variables/type_of_variable.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class type_of_variable {
+
+	public static void main(String[] args) {
+		int age;
+		age = 24;
+		int result = age + 10;
+		System.out.println(result);
+
+	}
+
+}
