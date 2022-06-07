0119f02dad00afc8143901d318b321902d6dd550
duck_ach <djagmlfk@naver.com>
2022-06-07 20:13:19 +0900

    clear

---
+++ chap02/src/sec01/exam01/VariableInitializationExample.java
@@ -0,0 +1,17 @@
+package sec01.exam01;
+
+public class VariableInitializationExample {
+
+	public static void main(String[] args) {
+		//변수 value 선언 //초기화를 진행해주어야 에러가 뜨지않
+		int value = 0;
+		
+		//변수 value 값을 얻고 10을 더하는 산술 연산을 수행
+		//연산의 결과값을 변수 result의 초기값으로 저장 
+		int result = value + 10 ;
+		
+		//변수 result 값을 읽고 콘솔에 출력
+		System.out.println(value);
+	}
+
+}
--- chap1/src/sec03/exam01/Hello.java
+++ chap1/src/sec03/exam01/Hello.java
@@ -1,5 +1,9 @@
 package sec03.exam01;
 
+/*
+ * 작성자 : 엄희라 
+ * 작성일 : 2022.06.07
+ */
 public class Hello {
 // 프로그램 실행 진입점 
 	public static void main(String[] args) {
