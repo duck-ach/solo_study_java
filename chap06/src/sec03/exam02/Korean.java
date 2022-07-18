e16f873ed6e065ef3740d748b0f8a0cd5d150b17
duck_ach <djagmlfk@naver.com>
2022-07-18 23:02:07 +0900

    필드 초기화

---
+++ chap06/src/sec03/exam02/Korean.java
@@ -0,0 +1,15 @@
+package sec03.exam02;
+
+public class Korean {
+	//field
+	String nation = "대한민국";
+	String name;
+	String ssn;
+	
+	//Constructor
+	public Korean(String n, String s) {
+		name = n;
+		ssn = s;
+	}
+
+}
